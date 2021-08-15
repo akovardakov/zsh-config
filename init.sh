83f303b09e11e0bd5ced61621964ffb60ef58e5c
Alexey Kovardakov <alexey.kovardakov@gmail.com>
2021-08-15 20:39:40 +0300

    new aliases

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
-alias ldgr="ledger -f ~/ledger/main.dat"
 setopt autolist
 
 ZSH_THEME="agnoster"
 
-plugins=(git)
+plugins=(git tmux)
 
 export ZSH="$ZSH_CONFIG/oh-my-zsh"
 export ZSH_CACHE_DIR="$XDG_CACHE_HOME/zsh"
@@ -12,6 +11,10 @@ source $ZSH/oh-my-zsh.sh
 source $ZSH_CONFIG/zsh-you-should-use/you-should-use.plugin.zsh
 source $ZSH_CONFIG/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-export PATH="/usr/local/opt/python@3.7/bin:$PATH"
+export PATH="/usr/local/opt/python@2.7/bin:$PATH"
 
-alias executer='EXECUTER_CONF=~/.config/executer/main.conf /usr/bin/perl /usr/local/bin/executer'
+alias ldgr="ledger"
+alias emx="emacsclient -c -n -a ''"
+alias hgrep="history | grep"
+
+source $ZSH_CONFIG/ya.sh
--- zshrc
+++ zshrc
@@ -1,3 +1,4 @@
+fpath+=~/.config/zsh/arc
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
@@ -8,5 +9,9 @@ export ZSH_CONFIG="$XDG_CONFIG_HOME/zsh"
 source $ZSH_CONFIG/init.sh
 
 export WORKON_HOME=$HOME/Work/venvs
-export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 source /usr/local/bin/virtualenvwrapper.sh
+
+if [ -f $(brew --prefix)/etc/zsh_completion ]; then
+. $(brew --prefix)/etc/zsh_completion
+fi
